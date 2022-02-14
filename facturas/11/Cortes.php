9804bb0046e6361a66bbcdd27d1585f49bb69688
aericknunez <aerick.nunez@gmail.com>
2022-02-14 16:09:28 -0600

    FACTURAS

---
+++ facturas/11/Cortes.php
@@ -0,0 +1,111 @@
+ <?php
+
+use Mike42\Escpos\EscposImage;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+use Mike42\Escpos\Printer;
+
+
+
+class CorteDeCaja {
+
+
+/*
+    Corte de caja
+*/
+
+
+public function CortePrint($data, $printer){
+    $doc = new Documentos();
+
+    $connector = new WindowsPrintConnector($printer);
+    $printer = new Printer($connector);
+    $printer -> initialize();
+    
+    $printer -> setFont(Printer::FONT_B);
+    
+    $printer -> setTextSize(1, 2);
+    $printer -> setLineSpacing(80);
+    
+    $printer -> setJustification(Printer::JUSTIFY_CENTER);
+    
+    $printer -> text("RESUMEN DE CORTE DE CAJA");
+    $printer->feed();
+    
+    
+    $printer -> text("_______________________________________________________");
+    $printer->feed();
+    
+    $printer -> text("CAJERO: " . $data['cajero']);
+    $printer->feed();
+    $printer -> text($doc->DosCol("CAJA: ", 40, $data['caja'], 10));
+    $printer->feed();
+
+       
+    
+    $printer -> text($doc->DosCol("TOTAL DE VENTA: ", 40, Helpers::Dinero($data['total_venta']), 10));
+    
+
+
+      
+    $printer -> text("_______________________________________________________");
+    $printer->feed();
+    
+    
+
+    
+    
+    
+    $printer -> text($doc->DosCol("GASTOS REGISTRADOS: ", 40, Helpers::Dinero($data['gastos']), 10));
+    
+    
+    $printer -> text($doc->DosCol("REMESAS: ", 40, Helpers::Dinero($data['remesas']), 10));
+
+
+    $printer -> text($doc->DosCol("ABONOS: ", 40, Helpers::Dinero($data['abonos']), 10));
+    
+    
+    $printer -> text("__________________________________________________");
+    $printer->feed();
+    
+    
+    
+    
+    $printer -> text($doc->DosCol("DINERO EN APERTURA: ", 40, Helpers::Dinero($data['efectivo_inicial']), 10));
+    
+    $printer -> text($doc->DosCol("EFECTIVO INGRESADO: ", 40, Helpers::Dinero($data['efectivo_final']), 10));
+
+    $printer -> text($doc->DosCol("EFECTIVO DEBIDO: ", 40, Helpers::Dinero($data['total_efectivo']), 10));
+
+    
+    
+    $printer -> text($doc->DosCol("DIFERENCIA: ", 40, Helpers::Dinero($data['diferencia']), 10));
+    
+    $printer -> text("_______________________________________________________");
+    $printer->feed();
+    
+    
+    
+    
+    
+    $printer->feed();
+    $printer->cut();
+    $printer->close();
+      
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}// class
---
+++ facturas/11/Impresiones.php
@@ -0,0 +1,51 @@
+ <?php  
+
+class Impresiones {
+
+
+public function Factura($data){
+    $doc = new Facturas();
+    $doc->ImprimirFactura($data);
+}
+
+// Solo peara la precuenta del cliente en termico
+public function PreCuenta($data){
+    $doc = new Precuenta();
+    if ($data['caja'] == 1) {
+        $printer = "LR200";   
+      } 
+      if ($data['caja'] == 2) {
+          $printer = "TICKET2"; 
+      } 
+    $doc->PrecuentaPrint($data, $printer);
+}
+
+// comandas para cocina a diferente panel
+public function Comanda($data){
+    $doc = new Comandas();
+    $doc->ImprimirComanda($data);
+}
+
+// comandas borradas para la cocina de diferente panel
+public function ComandaBorrada($data){
+    $doc = new Comandas();
+    $doc->ImprimirComanda($data);
+}
+
+// solo abrir caja segun el cajero
+public function AbrirCaja($data){
+    $doc = new Facturas();
+    $doc->AbreCaja($data);
+}
+
+
+// Corte de Cja
+public function Corte($data){
+    $doc = new CorteDeCaja();
+    $printer = "LR200";
+    $doc->CortePrint($data, $printer);
+}
+
+
+
+}// class
