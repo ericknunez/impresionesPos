f7f1c028a94a9b74d062b8d91ad7e3c32f0c3919
aericknunez <aerick.nunez@gmail.com>
2022-03-30 17:01:20 -0600

    Variables

--- configRemote.js
+++ configRemote.js
@@ -1,3 +1,8 @@
+// Url del server remoto
 const url_remote = "https://posrestaurant.hibridosv.com/api/impresiones";
+
+// Url del direcctorio donde se encuentran los archivos de impresion localmente
 const url_local = "http://192.168.1.7/impresiones/remote.php";
+
+// td del sistema a invocar
 const url_td = "0";
