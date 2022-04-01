881d34ffe4ed664732059d99c19458c924498f75
aericknunez <aerick.nunez@gmail.com>
2022-04-01 16:24:43 -0600

    Impresiones

--- configRemote.js
+++ configRemote.js
@@ -1,8 +1,9 @@
+// Las Url no deben terminar con / 
 // Url del server remoto
-const url_remote = "https://posrestaurant.hibridosv.com/api/impresiones";
+const url_remote = "https://posrestaurant.hibridosv.com";
 
 // Url del direcctorio donde se encuentran los archivos de impresion localmente
-const url_local = "http://192.168.1.7/impresiones/remote.php";
+const url_local = "http://192.168.1.7";
 
 // td del sistema a invocar
 const url_td = "0";
