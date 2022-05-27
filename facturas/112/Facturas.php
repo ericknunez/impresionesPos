c8b1b3b145bb7041dd02fa4d44ee03ae6ea903ae
DMartinez0 <douglasalexandermartinez@gmail.com>
2022-05-26 21:08:38 -0600

    Mandala

---
+++ facturas/112/Facturas.php
@@ -0,0 +1,219 @@
+ <?php
+
+use Mike42\Escpos\EscposImage;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+use Mike42\Escpos\Printer;
+
+
+
+class Facturas {
+
+
+/*
+0 Ninguno
+1 Ticket
+2 Facturas
+3 CCF
+4 NS
+*/
+
+public function ImprimirFactura($data){
+    // $data['documento_factura'] = 0; // maneja el tipo de documento a imprimir
+    $printer = "TICKET";
+    if ($data['documento_factura'] == 0) {
+        $this->Ninguno();
+    }
+    if ($data['documento_factura'] == 1) {
+        $this->Ticket($data, $printer);
+    }
+    if ($data['documento_factura'] == 2) {
+        // $this->Factura();
+        $this->Ticket($data, $printer);
+    }
+}
+
+
+public function Ninguno(){
+  $this->AbreCaja();
+}
+
+
+
+public function Ticket($data, $printer){
+    $doc = new Documentos();
+    
+    $img  = "C:/laragon/www/impresiones/facturas/112/img/logo.jpg";
+  
+  $connector = new WindowsPrintConnector($printer);
+  $printer = new Printer($connector);
+  $printer -> initialize();
+  
+  $printer->pulse();
+
+  $printer -> setFont(Printer::FONT_B);
+  
+  $printer -> setTextSize(1, 2);
+  $printer -> setLineSpacing(80);
+  
+  
+  $printer -> setJustification(Printer::JUSTIFY_CENTER);
+  $logo = EscposImage::load($img, false);
+  $printer->bitImage($logo);
+  $printer -> setJustification(Printer::JUSTIFY_CENTER);
+//   $printer->text($data['empresa_nombre']);
+  
+$printer->text("Plaza constitucion local # 16 fte. a");
+$printer->feed();
+
+$printer->text("Parque central de Metapan");
+$printer->feed();
+
+$printer -> setJustification(Printer::JUSTIFY_LEFT);
+$printer->text("Tel: 7674-2249");
+$printer->feed();
+
+  
+  $printer->text("TICKET NUMERO: " . $data['no_factura']);
+  $printer->feed();
+  $printer -> text($doc->DosCol($data['fecha'], 0, $data['hora'], 10));
+
+  
+  
+  /* Stuff around with left margin */
+  $printer->feed();
+  $printer -> setJustification(Printer::JUSTIFY_CENTER);
+  $printer -> text("________________________________________________________");
+  $printer -> setJustification(Printer::JUSTIFY_LEFT);
+  $printer->feed();
+  /* Items */
+  
+  $printer -> setJustification(Printer::JUSTIFY_LEFT);
+  $printer -> setEmphasis(true);
+  $printer -> text($doc->Item("Cant", 'Producto', 'Precio', 'Total'));
+  $printer -> setEmphasis(false);
+  
+  
+
+  foreach ($data['productos'] as $producto) {
+        $printer -> text($doc->Item($producto['cant'], $producto["producto"], Helpers::Format($producto["pv"]), Helpers::Format($producto["total"]))); 
+  }
+  
+   
+  $printer -> text("________________________________________________________");
+  $printer->feed();
+  
+  
+  
+  $printer -> text($doc->DosCol("Sub Total " . $data['tipo_moneda'] . ":", 40, Helpers::Format($data['total']), 10));
+  
+  
+  
+  if ($data['propina_cant']) {
+    $printer -> text($doc->DosCol("Propina " . $data['tipo_moneda'] . ":", 40, Helpers::Format($data['propina_cant']), 10));
+  }
+
+  $printer -> setEmphasis(true);
+  $printer -> text($doc->DosCol("Total " . $data['tipo_moneda'] . ":", 40, Helpers::Format($data['propina_cant'] + $data['total']), 10));
+  $printer -> setEmphasis(false);
+  
+  
+  
+  $printer -> text("________________________________________________________");
+  $printer->feed();
+  
+
+    $printer -> text($doc->DosCol("Efectivo " . $data['tipo_moneda'] . ":", 40, Helpers::Format($data['efectivo']), 10));
+    $printer -> text($doc->DosCol("Cambio " . $data['tipo_moneda'] . ":", 40, Helpers::Format($data['cambio']), 10));
+    
+    
+    $printer -> text("________________________________________________________");
+    $printer->feed();
+  
+    
+
+  
+  
+  
+  
+  
+  $printer -> text("Cajero: " . $data['cajero']);
+  $printer->feed();
+  
+
+  if($data['cliente_nombre'] != NULL){
+    $printer -> text("Cliente: " . $data['cliente_nombre']);
+    $printer->feed();
+  }
+  if($data['cliente_direccion'] != NULL){
+    $printer -> text($data['cliente_direccion']);
+    $printer->feed();
+  }
+  if($data['cliente_telefono'] != NULL){
+    $printer -> text("Telefono: " . $data['cliente_telefono']);
+    $printer->feed();
+  }
+  
+  // datos del cliente delivery
+  
+  
+  // nombre de mesa
+  if($data['mesa']['nombre_mesa'] != NULL){
+    $printer -> text("Mesa: " . $data['mesa']['nombre_mesa']);
+    $printer->feed();
+  }
+  
+// llevar o comer aqui
+if($data['llevar_aqui'] != NULL){
+  if ($data['llevar_aqui'] == 1) {
+    $tipo = "LLevar";
+  } else {
+    $tipo = "Comer Aqui";
+  }
+  $printer -> text( $tipo);
+   $printer->feed();
+}
+
+
+  
+
+  $printer -> text("________________________________________________________");
+  $printer->feed();
+  
+  
+  $printer->feed();
+  $printer -> setJustification(Printer::JUSTIFY_CENTER);
+  $printer -> text("GRACIAS POR SU PREFERENCIA...");
+  $printer -> setJustification();
+  
+  
+  $printer->feed();
+  $printer->cut();
+  $printer->close();
+
+}
+
+
+public function Factura(){
+  $this->AbreCaja();
+}
+
+
+
+public function AbreCaja($datos){
+  $printer = "TICKET";
+
+  $connector = new WindowsPrintConnector($printer);
+  $printer = new Printer($connector);
+  $printer->pulse();
+  $printer->close();
+}
+
+
+
+
+
+
+
+
+
+}// class
