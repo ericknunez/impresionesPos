ad496ac5ea3e6d9a7eb9bf666e1ef27572f4c4d8
aericknunez <aerick.nunez@gmail.com>
2022-11-06 19:50:00 -0600

    Remote

--- remote.php
+++ remote.php
@@ -17,67 +17,67 @@ fwrite($file, json_encode($data));
 fclose($file);
 
 
-// if($data["identidad"] != NULL){
-// 	require_once ('src/autoload.php'); 
-// 	include_once 'facturas/'.$data["identidad"].'/Impresiones.php'; // maneja el documento a imprimir
-// 	include_once 'facturas/'.$data["identidad"].'/Documentos.php'; // configuraciones de impresion
-// 		$fac = new Impresiones();
+if($data["identidad"] != NULL){
+	require_once ('src/autoload.php'); 
+	include_once 'facturas/'.$data["identidad"].'/Impresiones.php'; // maneja el documento a imprimir
+	include_once 'facturas/'.$data["identidad"].'/Documentos.php'; // configuraciones de impresion
+		$fac = new Impresiones();
 
 		
-// 	if($data["tipo_impresion"] == 1){ /// solo la pre cuenta
-// 	include_once 'facturas/'.$data["identidad"].'/Precuenta.php'; // documento de precuenta
-// 		$fac->PreCuenta($data); 
-// 	}
+	if($data["tipo_impresion"] == 1){ /// solo la pre cuenta
+	include_once 'facturas/'.$data["identidad"].'/Precuenta.php'; // documento de precuenta
+		$fac->PreCuenta($data); 
+	}
 
-// 	if($data["tipo_impresion"] == 2){ /// comanda a cualquier panel
-// 	include_once 'facturas/'.$data["identidad"].'/Comandas.php'; // documento
-// 		$fac->Comanda($data); 
-// 	}
+	if($data["tipo_impresion"] == 2){ /// comanda a cualquier panel
+	include_once 'facturas/'.$data["identidad"].'/Comandas.php'; // documento
+		$fac->Comanda($data); 
+	}
 
-// 	if($data["tipo_impresion"] == 3){ /// factura, ticket, ccf, etc
-// 	include_once 'facturas/'.$data["identidad"].'/Facturas.php'; // documento de precuenta
-// 		$fac->Factura($data); 
-// 	}
+	if($data["tipo_impresion"] == 3){ /// factura, ticket, ccf, etc
+	include_once 'facturas/'.$data["identidad"].'/Facturas.php'; // documento de precuenta
+		$fac->Factura($data); 
+	}
 
-// 	if($data["tipo_impresion"] == 4){ // / comanda borrda a cualquier panel
-// 	include_once 'facturas/'.$data["identidad"].'/Comandas.php'; // documento
-// 		$fac->ComandaBorrada($data); 
-// 	}
+	if($data["tipo_impresion"] == 4){ // / comanda borrda a cualquier panel
+	include_once 'facturas/'.$data["identidad"].'/Comandas.php'; // documento
+		$fac->ComandaBorrada($data); 
+	}
 
-//     if($data["tipo_impresion"] == 5){ /// solo abre la caja
-// 	include_once 'facturas/'.$data["identidad"].'/Facturas.php'; // documento de precuenta
-// 		$fac->AbrirCaja($data); 
-// 	}
+    if($data["tipo_impresion"] == 5){ /// solo abre la caja
+	include_once 'facturas/'.$data["identidad"].'/Facturas.php'; // documento de precuenta
+		$fac->AbrirCaja($data); 
+	}
 
-// 	// cortes
-// 	if($data["tipo_impresion"] == 10){ /// Corte de Caja
-// 		include_once 'facturas/'.$data["identidad"].'/Cortes.php'; // documento de precuenta
-// 			$fac->Corte($data); 
-// 	}
-// 	if($data["tipo_impresion"] == 11){ /// Corte Z
-// 		include_once 'facturas/'.$data["identidad"].'/Cortes.php'; // documento de precuenta
-// 			$fac->CorteZ($data); 
-// 	}
-// 	if($data["tipo_impresion"] == 12){ /// Reporte Diario
-// 		include_once 'facturas/'.$data["identidad"].'/Cortes.php'; // documento de precuenta
-// 			$fac->ReporteDiario($data); 
-// 	}
-// }
-
-
+	// cortes
+	if($data["tipo_impresion"] == 10){ /// Corte de Caja
+		include_once 'facturas/'.$data["identidad"].'/Cortes.php'; // documento de precuenta
+			$fac->Corte($data); 
+	}
+	if($data["tipo_impresion"] == 11){ /// Corte Z
+		include_once 'facturas/'.$data["identidad"].'/Cortes.php'; // documento de precuenta
+			$fac->CorteZ($data); 
+	}
+	if($data["tipo_impresion"] == 12){ /// Reporte Diario
+		include_once 'facturas/'.$data["identidad"].'/Cortes.php'; // documento de precuenta
+			$fac->ReporteDiario($data); 
+	}
+}
 
 
 
-// $archivos = glob("archivos/*.json");  
-//   foreach($archivos as $data){ 
 
-//   	$archivo = str_replace("archivos/", "", $data);
-//   	$nombre = str_replace(".json", "", $archivo);
 
-// 	if ($nombre < (Helpers::TimeId() - 3600) and file_exists($data)) {
-// 		@unlink($data); 
-// 	} 
-// } 
+$archivos = glob("archivos/*.json");  
+  foreach($archivos as $data){ 
+
+  	$archivo = str_replace("archivos/", "", $data);
+  	$nombre = str_replace(".json", "", $archivo);
+
+	if ($nombre < (Helpers::TimeId() - 3600) and file_exists($data)) {
+		@unlink($data); 
+	} 
+} 
 
 
 
